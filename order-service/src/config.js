8a02473094c7144fdb2c215dbd00cd04bc10f810
rajni15 <rajni@joni.co.in>
2024-07-10 14:23:49 +0530

    Update config.js

--- order-service/src/config.js
+++ order-service/src/config.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3003,
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/order-service'
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/order-service',
+  productServiceUrl: process.env.PRODUCT_SERVICE_URL || 'http://localhost:3002'
 };
